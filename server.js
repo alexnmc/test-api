235fcddbabc41944a15c4425dccf4e2df7f8c216
alex <nemechekalexander@gmail.com>
2022-08-21 17:01:51 -0400

    initial commit

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.json())
 app.use("/test", require("./routes/test"))
 
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT} sir!`)
 })
